437373b12675178a618c4734270f126451bb18c7
= <=>
2018-10-01 23:27:58 +0700

    h

---
+++ CodeGame/src/main/java/LeanJava/App.java
@@ -0,0 +1,13 @@
+package LeanJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
